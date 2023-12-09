be49443bdd584fdb89bee20dc6aade019c7bf297
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-12-09 16:43:29 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int number1;
+    std::cout << "Enter the first number: ";
+    std::cin >> number1;
+
+    int number2;
+    std::cout << "Enter the second number: ";
+    std::cin >> number2;
+
+    int sum;
+    std::cout << "Enter their amount: ";
+    std::cin >> sum;
+
+    std::cout << "-----Checking-----" << std::endl;
+
+    if (number1 + number2 == sum)
+        std::cout << "Right!" << std::endl;
+    else
+        std::cout << "Error! Correct result: " << number1 + number2;
 }
